34d68525c3824b439207bf4cf546696c1073b2e5
Veronika <veronika@imac.local>
2016-11-10 14:36:46 +0400

    добавлены диалоговые окна

--- Task1/app/src/main/java/com/example/user/task1/RecyclerViewAdapter.java
+++ Task1/app/src/main/java/com/example/user/task1/RecyclerViewAdapter.java
@@ -1,5 +1,7 @@
 package com.example.user.task1;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -49,6 +51,67 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
       name = (TextView) itemView.findViewById(R.id.element_name);
       icon = (ImageView) itemView.findViewById(R.id.element_color);
       remove = (Button) itemView.findViewById(R.id.remove_element);
+
+      itemView.setOnClickListener(new SelectListener());
+      remove.setOnClickListener(new RemoveButtonListener());
+    }
+
+    private class SelectListener implements View.OnClickListener {
+
+      private Element e;
+
+      @Override
+      public void onClick(View v) {
+        selectDialog(v);
+      }
+
+      private void selectDialog(View v) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+        builder.setMessage(R.string.select_element);
+        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+          public void onClick(DialogInterface dialog, int id) {
+
+          }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+      }
+    }
+
+    private class RemoveButtonListener implements View.OnClickListener {
+
+      private Element e;
+
+      @Override
+      public void onClick(View v) {
+        removeDialog(v);
+      //  remove(e);
+      }
+
+      private void removeDialog(View v) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+        builder.setMessage(R.string.remove_element);
+        builder.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+          public void onClick(DialogInterface dialog, int id) {
+//            elements.remove(index);
+//            adapter.notifyDataSetChanged();
+//            listView.setSelection(0);
+          }
+        });
+        builder.setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+          public void onClick(DialogInterface dialog, int id) {
+
+          }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+      }
+    }
+
+    private void remove(Element e) {
+      int position = elements.indexOf(e);
+      elements.remove(position);
+      notifyItemRemoved(position);
     }
   }
 }
